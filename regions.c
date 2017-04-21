7fe58ca08ec5d2808c7646cbd9bf7a043f46789a
manlyunicorn <xivongolaboss@gmail.com>
2017-04-21 17:13:53 -0500

    Implement functions in regions.c
    from regions.h

--- regions.c
+++ regions.c
@@ -32,15 +32,130 @@ int count = 0;
 
 
 Boolean rinit(const char *region_name, r_size_t region_size){
-    chosen->space = chosen->space-roundedSize;
-    result = chosen->buffer;
+    if(region_size<=0){
+        return false;
+    }
+    int i = 0;
+    NodePtr test = head;
+    for(i=0;i<count;i++){
+        if(strcmp(region_name,test->name)==0){
+            return false;
+        }
+    }
+    NodePtr newNode = (NodePtr)malloc(sizeof(Node));
+    uchar temp[(r_size_t)(ceil((double)region_size/8.0)*8)];
+    newNode->buffer = temp;
+    newNode->head = NULL;
+    newNode->size =(r_size_t)(ceil((double)region_size/8.0)*8);
+    newNode->space = newNode->size ;
+    newNode->buffer = (uchar *)malloc(newNode->size);
+    char *tempCharArray = (char *)malloc(NAME_SIZE);
+    strcpy(tempCharArray, region_name);
+    newNode->name = tempCharArray;
+
+    if(count==0){
+        head = newNode;
+    }
+    else{
+        Node *temp = head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;    }
+    count++;
+    chosen = newNode;
+    return true;
+}
+Boolean rchoose(const char *region_name){
+    int i = 0;
+    NodePtr test = head;
+    for(i=0;i<count;i++){
+
+        if((strcmp(region_name,test->name)==0)){
+            chosen = test;
+            return true;
+        }
+        test = test->next;
+    }
+    return false;
+}
+const char *rchosen(){
+    if(chosen!=NULL){
+        return chosen->name;
+    }
+    else{
+    return NULL;
+    }
+}
+void *ralloc(r_size_t block_size){
+    void * result = NULL;
+    r_size_t roundedSize = (r_size_t)(ceil((double)block_size/8.0)*8);
+    if(chosen->space<block_size){
+        return NULL;
+    }
+    if(block_size==0||chosen==NULL){
+        return NULL;
+    }
+    if(chosen->size==chosen->space){
+        BlockNodePtr newNode = (BlockNodePtr)malloc(sizeof(BlockNode));
+        newNode->offset = 0;
+        newNode->next = NULL;
+        newNode->size = roundedSize;
+        newNode->spaceAfter = chosen->space-newNode->offset-newNode->size;
+        chosen->head = newNode;
+        chosen->space = chosen->space-roundedSize;
+        result = chosen->buffer;
+
+    }
+    else{
+        Boolean finished = false;
+    BlockNodePtr temp = chosen->head;
+        if(temp->offset>=roundedSize){
+            BlockNodePtr newNode = (BlockNodePtr)malloc(sizeof(BlockNode));
+            newNode->size = roundedSize;
+            newNode->spaceAfter = temp->offset-roundedSize;
+            newNode->next = temp;
+            result = chosen->buffer + newNode->offset;
+        }
+    while(temp!=NULL&&!finished){
+
+         if(temp->spaceAfter>=roundedSize){
+            finished = true;
+            chosen->space = chosen->space-block_size;
+            BlockNodePtr newNode = (BlockNodePtr)malloc(sizeof(BlockNode));
+            newNode->size = roundedSize;
+            newNode->offset = temp->offset + temp->size;
+            temp->spaceAfter = 0;
+            if(temp->next == NULL){
+                newNode->next = NULL;
+                newNode->spaceAfter = chosen->size-newNode->offset-newNode->size;
+            }
+            else{
+                newNode->next = temp->next;
+                newNode->spaceAfter = temp->next->offset-newNode->offset-newNode->size;
+
+            }
+            temp->next = newNode;
+            result = chosen->buffer + newNode->offset;
+        }
+        temp = temp->next;
+    }
+    }
 
     return result;
 }
 r_size_t rsize(void *block_ptr){
     void * ptr = NULL;
     if(chosen!=NULL){
-            //....
+        BlockNodePtr temp = chosen->head;
+        while(temp!=NULL){
+            ptr = NULL;
+            ptr =chosen->buffer+temp->offset;
+            if(ptr==block_ptr){
+                return temp->size;
+            }
+            temp = temp->next;
+        }
     }
     return 0;
 }
@@ -50,12 +165,26 @@ Boolean rfree(void *block_ptr){
         BlockNodePtr temp = chosen->head;
         BlockNodePtr tempB = NULL;
         while(temp!=NULL){
+            ptr = NULL;
+            ptr =chosen->buffer+temp->offset;
             if(ptr==block_ptr){
                 if(tempB!=NULL){
+                    tempB->spaceAfter = tempB->spaceAfter+temp->size;
+                    tempB->next = temp->next;
+                    chosen->space = chosen->space+temp->size;
+                    if(temp->next == NULL){
+                    }
+                    chosen->space = chosen->space+temp->size;
+                    free(temp);
                     return true;
                 }
                 else{
-                        //...
+                    chosen->head = temp->next;
+                    if(temp->next == NULL){
+                    }
+                    chosen->space = chosen->space+temp->size;
+                    free(temp);
+                    return true;
                 }
 
             }
@@ -63,13 +192,42 @@ Boolean rfree(void *block_ptr){
             temp = temp->next;
         }
     }
+    return false;
 }
 void rdestroy(const char *region_name){
     NodePtr tempB = NULL;
     NodePtr temp = head;
     Boolean finished = false;
     while(temp!=NULL&&!finished){
-            //...
+        if(strcmp(region_name,temp->name)==0){
+            count--;
+            if(tempB!=NULL){
+                tempB->next = temp->next;
+            }
+            else{
+                head = temp->next;
+            }
+            free(temp->name);
+            free(temp->buffer);
+            BlockNodePtr temp2 = temp->head;
+            BlockNodePtr temp2N = NULL;
+            if(temp2!=NULL){
+             temp2N = temp->head->next;
+            }
+            while(temp2!=NULL){
+                free(temp2);
+                temp2 = temp2N;
+                if(temp2N!=NULL){
+                    temp2N = temp2N->next;
+                }
+            }
+            free(temp);
+            finished = true;
+        }
+        else{
+        tempB = temp;
+        temp = temp->next;
+        }
     }
 }
 void rdump(){
@@ -79,6 +237,17 @@ void rdump(){
         printf("NULL\n");
     }
     else{
-            //...
+        int i = 0;
+        for(i=0;i<count;i++){
+        printf("\nRegion: %s\tsize: %d\tfree space: %d percent\n", temp->name, temp->size,temp->space*100/temp->size);
+        BlockNodePtr tempBlock = temp->head;
+        int counter = 0;
+        while(tempBlock!=NULL){
+            printf("Block Number: %d\t size: %d\n", counter,tempBlock->size);
+            counter++;
+            tempBlock = tempBlock->next;
+        }
+        temp = temp->next;
+    }
     }
 }
